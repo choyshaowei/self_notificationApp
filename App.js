0647bb66bd51fa40a7925b015ed809e35486b5c1
choyshaowei <choy950721@gmail.com>
2020-12-01 15:15:10 +0800

    Passed Notification Token

--- App.js
+++ App.js
@@ -1,35 +1,88 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { useEffect } from 'react';
-import { StyleSheet, Button, View } from 'react-native';
-import * as Notifications from 'expo-notifications';
-import * as Permissions from 'expo-permissions';
+import { StatusBar } from "expo-status-bar";
+import React, { useEffect, useState } from "react";
+import { StyleSheet, Button, View } from "react-native";
+import * as Notifications from "expo-notifications";
+import * as Permissions from "expo-permissions";
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => {
+    return {
+      shouldShowAlert: true,
+    };
+  },
+});
 
 export default function App() {
+  const [pushToken, setPushToken] = useState();
   useEffect(() => {
     Permissions.getAsync(Permissions.NOTIFICATIONS)
       .then((statusObj) => {
-        if (statusObj.status !== 'granted') {
+        if (statusObj.status !== "granted") {
           return Permissions.askAsync(Permissions.NOTIFICATIONS);
         }
         return statusObj;
       })
       .then((statusObj) => {
-        if (statusObj.status !== 'granted') {
-          return;
+        if (statusObj.status !== "granted") {
+          throw new Error("Permission not granted");
         }
+      })
+      .then(() => {
+        return Notifications.getExpoPushTokenAsync();
+      })
+      .then((response) => {
+        const token = response.data;
+        console.log(token);
+        setPushToken(token);
+      })
+      .catch((err) => {
+        return null;
       });
   }, []);
 
-  const triggerNotificationHandler = () => {
-    Notifications.scheduleNotificationAsync({
-      content: {
-        title: 'My first local notification',
-        body: 'This is the first local notification we are sending!',
+  useEffect(() => {
+    const backgroundSubscription = Notifications.addNotificationResponseReceivedListener(
+      (response) => {
+        console.log(response);
+      }
+    );
+    const foregroundSubscription = Notifications.addNotificationReceivedListener(
+      (notification) => {
+        // console.log(notification);
+      }
+    );
+    return () => {
+      backgroundSubscription.remove();
+      foregroundSubscription.remove();
+    };
+  }, []);
+
+  const triggerNotificationHandler = async () => {
+    // Notifications.scheduleNotificationAsync({
+    //   content: {
+    //     title: "My first local notification",
+    //     body: "This is the first local notification we are sending!",
+    //     data: { specialData: "Some Text" },
+    //   },
+    //   trigger: {
+    //     seconds: 5,
+    //   },
+    // });
+    const response = await fetch("https://exp.host/--/api/v2/push/send", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Accept-Encoding": "gzip, deflate",
+        "Content-Type": "application/json",
       },
-      trigger: {
-        seconds: 5,
-      },
+      body: JSON.stringify({
+        to: pushToken,
+        data: { extraData: "Some Data" },
+        title: "Sent via App",
+        body: "This push notification was sent via the app!",
+      }),
     });
+    console.log(response);
   };
 
   return (
@@ -46,8 +99,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
